bbeb739c2349ec7662296737b5fdcc40358766fe
Craig Blakeney <craigblakeney@gmail.com>
2018-02-14 16:29:11 +0000

    Created Tune class and added private fields

---
+++ ie/dit/Tune.java
@@ -0,0 +1,17 @@
+package ie.dit;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+
+public class Tune
+{
+	private int x;
+	private String title;
+	private String altTitle;
+	private String notation;
+
+}
+
+	
